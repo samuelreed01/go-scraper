4d265affd5f863feaf3ecfbece6322729b6f1f2d
Samuel Reed <samuelreed01@gmail.com>
2026-01-22 15:24:38 +0700

    test cold start times

--- scraper.go
+++ scraper.go
@@ -10,11 +10,14 @@ import (
 
 // Response structure
 type ScrapeResult struct {
-	Text  string   `json:"text"`
-	Images int `json:"images"`
-	Heading int `json:"headings"`
-	Paragraphs int `json:"paragraphs"`
-	Words int `json:"words"`
+	Text         string `json:"text"`
+	Images       int    `json:"images"`
+	Heading      int    `json:"headings"`
+	Paragraphs   int    `json:"paragraphs"`
+	Words        int    `json:"words"`
+	StartupTime  int64  `json:"startup_time"`
+	NavigateTime int64  `json:"navigate_time"`
+	CompleteTime int64  `json:"complete_time"`
 }
 
 // Request structure
@@ -24,6 +27,7 @@ type ScrapeRequest struct {
 
 func Scrape(url string) (*ScrapeResult, error) {
 	// Context with timeout
+	startTime := time.Now()
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
@@ -47,9 +51,20 @@ func Scrape(url string) (*ScrapeResult, error) {
 	var paragraphCount int
 	var headingsCount int
 
+	var startup time.Duration
+	var navigate time.Duration
+
 	err := chromedp.Run(taskCtx,
 		chromedp.Navigate(url),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			startup = time.Since(startTime)
+			return nil
+		}),
 		chromedp.WaitVisible("body", chromedp.ByQuery),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			navigate = time.Since(startTime)
+			return nil
+		}),
 		chromedp.Text("body", &pageText, chromedp.NodeVisible, chromedp.ByQuery),
 		chromedp.EvaluateAsDevTools(`
 			document.querySelectorAll("h1,h2,h3,h4,h5,h6").length
@@ -68,10 +83,13 @@ func Scrape(url string) (*ScrapeResult, error) {
 	wordCount := len(strings.Fields(pageText))
 
 	return &ScrapeResult{
-		Text:  pageText,
-		Images: imgCount,
-		Heading: headingsCount,
-		Paragraphs: paragraphCount,
-		Words: wordCount,
+		Text:         pageText,
+		Images:       imgCount,
+		Heading:      headingsCount,
+		Paragraphs:   paragraphCount,
+		Words:        wordCount,
+		StartupTime:  startup.Milliseconds(),
+		NavigateTime: navigate.Milliseconds(),
+		CompleteTime: time.Since(startTime).Milliseconds(),
 	}, nil
 }
